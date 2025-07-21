ee83bbdb6ebd7ad5b70ca30bea3badc8582755bc
tygara <evenoddvergara@gmail.com>
2025-07-20 23:21:43 -0700

    final touches

--- quiz3.asm
+++ quiz3.asm
@@ -1,27 +1,28 @@
 section .data
-  x db 5                 ; factorial to compute
-  buf db '000',10        ; three digit slots + newline
+  x db 5                            ; factorial to compute
+  buf db '000',10                   ; three digit slots + newline
 
 section .text
     global _start
 
 _start:
 ; compute factorial in EAX register
-    movzx ecx, byte [x]     ; counter = x
-    mov   eax, 1            ; accumulator = 1
+    xor ecx, ecx                    
+    mov cl, [x]                     ; load counter 
+    mov eax, 1                      ; accumulator = 1
 
 .calc:
     cmp   ecx, 1
-    jle   .print            ; jump if less or equal operation
-    mul   ecx               ; EAX *= ECX
+    jle   .print                  ; jump if less or equal operation
+    mul   ecx                     ; EAX *= ECX
     dec   ecx
-    jmp   .calc             ; jump back up to .calc
+    jmp   .calc                   ; jump back up to .calc
 
 .print:
     ; extract hundreds digit
     mov   ebx, 100
     xor   edx, edx
-    div   ebx               ; EAX=hundreds, EDX=remainder
+    div   ebx                     ; EAX=hundreds, EDX=remainder
     add   al, '0'
     mov   [buf], al
 
@@ -29,7 +30,7 @@ _start:
     mov   eax, edx
     mov   ebx, 10
     xor   edx, edx
-    div   ebx               ; EAX=tens, EDX=ones
+    div   ebx                     ; EAX=tens, EDX=ones
     add   al, '0'
     mov   [buf+1], al
 
@@ -38,13 +39,12 @@ _start:
     mov   [buf+2], dl
 
     ; 5) write 4 bytes (3 digits + newline)
-    mov   eax, 4            ; sys_write
-    mov   ebx, 1            ; stdout
+    mov   eax, 4                  ; sys_write
+    mov   ebx, 1                  ; stdout
     mov   ecx, buf
     mov   edx, 4
     int   0x80
 
-    mov     eax, 1            ; sys_exit
+    mov     eax, 1                ; sys_exit
     xor     ebx, ebx
     int     0x80
-
